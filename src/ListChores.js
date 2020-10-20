6e36fd81016eea9d6945b3dc5d7f779488df1a89
Nick Baumer <nick.baumer@gmail.com>
2020-10-20 12:45:45 +0100

    Updated using more modern packages. Commit prior to changes based on react course.

--- src/ListChores.js
+++ src/ListChores.js
@@ -1,51 +1,82 @@
-import React, { Component } from 'react';
-import { Button, ButtonToolbar } from 'react-bootstrap';
-
-  const buttonsInstance = (
-    <ButtonToolbar>
-      <Button bsStyle="success">Add chore</Button>
-    </ButtonToolbar>
-    );
+import React, { Component } from "react";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 class ListChores extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
+    this.state = {
+      chore: "",
+      description: "",
+      when: new Date().getTime(),
+      token: props.token,
+    };
+  }
 
-      super(props);
-		
-      this.state = {
-         data: 'Chore here...',
-         when: (new Date).getTime()
-      }
+  updateChore = (event) => {
+    this.setState({ chore: event.target.value });
+  };
 
-      this.updateState = this.updateState.bind(this);
+  updateDesc = (event) => {
+    this.setState({ description: event.target.value });
+  };
 
-   };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post(
+        "http://localhost:3000/api/chore",
+        {
+          chore: this.state.chore,
+          description: this.state.description,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.state.token}`,
+          },
+        }
+      )
 
-   updateState(e) {
-      this.setState({data: e.target.value});
-   }
-   
-    handleSubmit(e) {
-        this.setState({data: 'you pressed it'});
-    }
+      .then(function (result) {
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    this.setState({ description: "" });
+    this.setState({ chore: "" });
+  };
 
-   
-     render() {
-      return (
-         <div>
-            <form action="https://chorescore-pljhll.c9users.io/api/chores" method="post" onSubmit={this.handleSubmit}>
-            <input type="text" name="name" value={this.state.data} 
-               onChange={this.updateState} /> 
-            <input type="text" name="who" />
-            <input type="hidden" name="when" value={this.state.when} />
-            <button>Submit</button>
-            </form>
-            <h4>{this.state.data}</h4>
-         </div>
-      );
-   }
-    
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <TextField
+            type="text"
+            name="chore"
+            value={this.state.chore}
+            onChange={this.updateChore}
+            label="Chore"
+            variant="outlined"
+          />
+          <TextField
+            type="text"
+            name="description"
+            value={this.state.description}
+            onChange={this.updateDesc}
+            label="More detail?"
+            variant="outlined"
+          />
+          <input type="hidden" name="when" value={this.state.when} />
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ListChores;
